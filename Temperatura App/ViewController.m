5015084b0dce2bbbff52bb10ef4e03078a389651
Alvaro Alva <7mesino.97@gmail.com>
2020-09-16 21:34:55 -0400

    Done

--- Temperatura App/ViewController.m
+++ Temperatura App/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *celciusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *fahrenheitLabel;
+@property (weak, nonatomic) IBOutlet UISlider *degreeSlider;
 
 @end
 
@@ -16,8 +19,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setValues];
 }
 
-
+- (IBAction)degreeSliderAdjust:(id)sender {
+    [self setValues];
+}
+-(float) calculateCelcius {
+    float result;
+    result = (self.degreeSlider.value -32) * 5.0/9.0;
+    
+    return result;
+}
+-(void) setValues {
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
+    [formatter setMaximumFractionDigits:1];
+    [formatter setRoundingMode:NSNumberFormatterRoundDown];
+    NSString * fahrenheitValue = [formatter stringFromNumber:[NSNumber numberWithFloat:self.degreeSlider.value]];
+    // set label value for celcius
+    
+    // set lavel value for fahrenheit
+    NSString * celciusValue = [formatter stringFromNumber:[NSNumber numberWithFloat:self.calculateCelcius]];
+    [self.celciusLabel setText:celciusValue];
+    [self.fahrenheitLabel setText:fahrenheitValue];
+}
 @end
